9c7f30dbc036f12b6403c1cec3be8bcd9d6573f4
mishra <jisumishra4@gmail.com>
2026-06-24 14:22:40 +0530

    PlugIn Talwind + Vite (Bundler Tool ) + DaisyUi (Component Library)

---
+++ README.md
@@ -0,0 +1,8 @@
+Create a Vite + React Project
+
+Installing Talwind Css :=
+npm install tailwindcss @tailwindcss/vite
+
+Using Component Library DaisyUi it is compatible with talwind>=
+npm i -D daisyui@latest
+@plugin "daisyui"; add This into the app.css
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+   return (
+    <div className="bg-red-500 text-white text-5xl p-10">
+      DevTinder
+    </div>
+  );
+  
+}
+
+// export default function App() {
+//   return (
+//     <div className="bg-red-500 text-white text-5xl p-10">
+//       DevTinder
+//     </div>
+//   );
+// }
+
+export default App
