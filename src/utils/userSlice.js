fd1d8e0f208ca99e8a3d35e574be4ada0e67b952
mishra <jisumishra4@gmail.com>
2026-06-26 16:19:51 +0530

    Adding A data in the reducers or adding a data in the slice reducers

--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -4,7 +4,12 @@ const userSlice = createSlice( {
   name : 'user',
   initialState : null,
   reducers : {
-    
+    addUser: (state, action ) => {
+      return action.payload;
+    },
+    removeUser: (state, action) => {
+     return null;
+    }
   }
 });
 
