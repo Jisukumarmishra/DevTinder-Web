3109a321d9e68fd0b5341b3fd4d98c6278816176
mishra <jisumishra4@gmail.com>
2026-06-28 02:35:19 +0530

    Complete The fetchUser API

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
+import { Axios } from "axios"
+import { BASE_URL } from "../utils/constants"
 
 
 const Body = () => {
+  const fetchUser = async () => {
+    try {const user = await Axios.GET(BASE_URL+"/profile/view", {
+      withCredential : true,
+    });}
+    catch (err) {
+     console.error(err); 
+    }
+
+  }
   return (
     <div>
       <NavBar/>
