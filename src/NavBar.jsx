294ade665d279f41f251a9a933e62d8c667303e4
mishra <jisumishra4@gmail.com>
2026-06-27 15:17:46 +0530

    Subsribing To The Store

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux"
+
 const NavBar = () => {
+  const user = useSelector((store) => store.user);
   return (
      <div className="navbar bg-base-300 shadow-sm">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">DevTinder</a>
   </div>
   <div className="flex gap-2">
-    <div className="dropdown dropdown-end mx-5">
+    {user && <div className="dropdown dropdown-end mx-5">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
@@ -25,7 +28,7 @@ const NavBar = () => {
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
       </ul>
-    </div>
+    </div>}
   </div>
     </div>
   )
