42a54d8477e073ed549cfc639ee56e231e7089a2
mishra <jisumishra4@gmail.com>
2026-06-28 17:29:09 +0530

    Use Link tag That Comes From React Router DOM Instead of Anchor Tag

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
@@ -6,7 +7,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm px-8">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">DevTinder</a>
+        <Link className="btn btn-ghost text-xl">DevTinder</Link>
       </div>
 
       {user && (
@@ -31,13 +32,13 @@ const NavBar = () => {
 
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
 
               <li>
