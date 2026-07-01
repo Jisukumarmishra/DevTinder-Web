7ab025992b8604f688e3d5ff79436cfd65b89a49
mishra <jisumishra4@gmail.com>
2026-07-01 17:01:32 +0530

    Dispatch An Action To Add the feed Data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,15 @@
 import axios from "axios"
 import { BASE_URL } from "../utils/constants"
+import { useDispatch } from "react-redux"
+import { addFeed } from "../utils/feedSlice"
 
 const Feed = () => {
+  const dispatch = useDispatch()
 
   const getFeed = async () => {
 
    const res = await axios.get(BASE_URL + "/feed");
+   dispatch(addFeed(res.data))
    
   }
   return (
