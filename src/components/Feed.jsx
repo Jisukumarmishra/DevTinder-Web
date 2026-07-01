dd62a1da2e1ea9cb4b96c09720a30be00f7916a3
mishra <jisumishra4@gmail.com>
2026-07-01 17:29:41 +0530

    Make An Api Call getFeed To Render The Page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,17 +1,29 @@
 import axios from "axios"
 import { BASE_URL } from "../utils/constants"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { addFeed } from "../utils/feedSlice"
+import { useEffect } from "react"
 
 const Feed = () => {
   const dispatch = useDispatch()
+  // read the feed
+  const feed = useSelector((store) => store.feed);
+  console.log(feed);
 
   const getFeed = async () => {
+    if(feed) return;
 
-   const res = await axios.get(BASE_URL + "/feed");
+   try {const res = await axios.get(BASE_URL + "/feed", {withCredentials : true});
    dispatch(addFeed(res.data))
-   
+  } catch (err) {
+    // Handle Error
   }
+
+  }
+
+  useEffect(() => {
+   getFeed();
+  }, [] )
   return (
     <div>Feed</div>
   )
