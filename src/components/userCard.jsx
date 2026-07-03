a9af70d76140ad21e3307379ff7f199136e9b57c
mishra <jisumishra4@gmail.com>
2026-07-03 16:20:54 +0530

    Build An User Card on Feed

--- src/components/userCard.jsx
+++ src/components/userCard.jsx
@@ -3,10 +3,7 @@ const UserCard = ({ user }) => {
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
       <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
+        <img src={user.photoUrl} alt="photo" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">Card Title</h2>
