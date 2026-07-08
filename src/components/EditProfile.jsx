2ecfdf833f85df685b74029741b44eee25763c54
mishra <jisumishra4@gmail.com>
2026-07-08 21:29:19 +0530

    Write the empty tag and start building the feature for the how my card show the other user of after editing , using reusing of userCard component here

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -8,119 +8,126 @@ const EditProfile = ({ user }) => {
   const [gender, setGender] = useState(user.gender);
   const [about, setabout] = useState(user.about);
   return (
-    <div className="flex justify-center my-10">
-      <div className="card card-border bg-base-300 w-96">
-        <div className="card-body">
-          <h2 className="card-title">Edit Profile</h2>
+    <>
+      <div className="flex justify-center my-10">
+        <div className="card card-border bg-base-300 w-96">
+          <div className="card-body">
+            <h2 className="card-title">Edit Profile</h2>
 
-          {/* Email */}
-          <label className="input validator mb-1">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
+            {/* Email */}
+            <label className="input validator mb-1">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
               >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-              </g>
-            </svg>
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
 
-            <input
-              type="email"
-              value={firstName}
-              onChange={(e) => setFirstname(e.target.value)}
-              placeholder="mail@site.com"
-              required
-            />
-          </label>
-          <label className="input validator mb-1">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
+              <input
+                type="email"
+                value={firstName}
+                onChange={(e) => setFirstname(e.target.value)}
+                placeholder="mail@site.com"
+                required
+              />
+            </label>
+            <label className="input validator mb-1">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
               >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-              </g>
-            </svg>
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
 
-            <input
-              type="email"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              placeholder="mail@site.com"
-              required
-            />
-          </label>
+              <input
+                type="email"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="mail@site.com"
+                required
+              />
+            </label>
 
-          <p className="validator-hint hidden mb-2">
-            Enter valid email address
-          </p>
+            <p className="validator-hint hidden mb-2">
+              Enter valid email address
+            </p>
 
-          {/* Password */}
-          <label className="input validator mt-2">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
+            {/* Password */}
+            <label className="input validator mt-2">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
               >
-                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-              </g>
-            </svg>
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                  <circle
+                    cx="16.5"
+                    cy="7.5"
+                    r=".5"
+                    fill="currentColor"
+                  ></circle>
+                </g>
+              </svg>
 
-            <input
-              type="password"
-              // value={}
-              onChange={(e) => setPassWord(e.target.value)}
-              placeholder="Password"
-              required
-              minLength={8}
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-            />
-          </label>
+              <input
+                type="password"
+                // value={}
+                onChange={(e) => setPassWord(e.target.value)}
+                placeholder="Password"
+                required
+                minLength={8}
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              />
+            </label>
 
-          <p className="validator-hint hidden">
-            Must be more than 8 characters, including
-            <br />
-            At least one number
-            <br />
-            At least one lowercase letter
-            <br />
-            At least one uppercase letter
-          </p>
+            <p className="validator-hint hidden">
+              Must be more than 8 characters, including
+              <br />
+              At least one number
+              <br />
+              At least one lowercase letter
+              <br />
+              At least one uppercase letter
+            </p>
 
-          <div className="card-actions justify-end">
-            <p className="text-red-500 p-4">{error}</p>
-            <button className="btn btn-primary my-3" onClick={handleLogin}>
-              Save Profile !!
-            </button>
+            <div className="card-actions justify-end">
+              <p className="text-red-500 p-4">{error}</p>
+              <button className="btn btn-primary my-3" onClick={handleLogin}>
+                Save Profile !!
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
