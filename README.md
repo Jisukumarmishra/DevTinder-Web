0886d1ae9473995c3b7262b6ee9afa8df3519771
mishra <jisumishra4@gmail.com>
2026-07-11 09:53:05 +0530

    Add connections page/compoennt and render it in the app.jsx0

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ Using Component Library DaisyUi it is compatible with talwind>=
 npm i -D daisyui@latest
 @plugin "daisyui"; add This into the app.css
 
-Install react routing dom package for the routing 
+Install react routing dom package for the routing
 Routing is Root level of An Application
 
 instal The cors package in backend To Sove The Cors proeblem
-add middleware of the cors in the backend with the credentials and origin to extract the token from the cookies and user in the login browser 
-
+add middleware of the cors in the backend with the credentials and origin to extract the token from the cookies and user in the login browser
 
 # Install Redux ToolKit To Stroe The user Data In Redux Store
+
 install the react-redux+@reduxjs/toolkit -----> configureStore -----> Provider (Provide A Store in The Application(app.jsx))------->createSlice And Export Things Properly(actions export)-----------> add Reducer to the store
+
+// now create page to see all my connections
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Body from "./components/Body"
-import LogIn from "./components/LogIn"
-import Profile from "./components/Profile"
-import { Provider } from "react-redux"
-import appStore from "./utils/appStore"
-import Feed from "./components/Feed"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import LogIn from "./components/LogIn";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
 
 function App() {
-   return (
+  return (
     <>
-    <Provider store = {appStore}>
-    <BrowserRouter basename="/">
-     <Routes>
-      <Route path="/" element={<Body/>}>
-       <Route path="/" element={<Feed/>}/>
-       <Route path="/login" element={<LogIn/>}/>
-       <Route path ="/profile" element={<Profile/>}/>
-      </Route>
-     </Routes>
-    </BrowserRouter>
-    </Provider>
-
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
-  
+  );
 }
 
 // export default function App() {
@@ -34,4 +35,4 @@ function App() {
 //   );
 // }
 
-export default App
+export default App;
