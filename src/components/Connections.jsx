0316fd5144e0523f38dd44bbf2048b338f0506d2
mishra <jisumishra4@gmail.com>
2026-07-11 13:40:11 +0530

    make an get api call using axios

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,7 +1,18 @@
+import axios from "axios";
 import React from "react";
+import { BASE_URL } from "../utils/constants";
 
 const Connections = () => {
-  return <div>Connections</div>;
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/connections", {
+        withCredentials: true,
+      });
+    } catch (err) {
+      // handle erroe case
+    }
+  };
+  return <div>// fetch the data</div>;
 };
 
 export default Connections;
