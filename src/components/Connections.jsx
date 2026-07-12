60be2b7f2cde21a31c9a52864fd8cd25596a107d
mishra <jisumishra4@gmail.com>
2026-07-12 16:11:26 +0530

    handle no connections data and there is not any connections

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -22,6 +22,11 @@ const Connections = () => {
   useEffect(() => {
     fetchConnections();
   }, []);
+
+  if (!connections) return;
+
+  if (connections.length === 0) return <h1>No Connections Found </h1>;
+
   return (
     <div className=" flex justify-center my-10">
       <h1 className="text-bold text-2xl">Connections</h1>
