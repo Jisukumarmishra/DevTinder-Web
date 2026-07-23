592ba3f58743bf141fb966799d5f99530b6abafe
mishra <jisumishra4@gmail.com>
2026-07-23 19:20:16 +0530

    Add svg icon graphics

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -46,14 +46,14 @@ const LogIn = () => {
               viewBox="0 0 24 24"
             >
               <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
                 stroke="currentColor"
+                strokeWidth="2"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                <circle cx="12" cy="8" r="4" />
+                <path d="M4 20c0-4 3.5-7 8-7s8 3 8 7" />
               </g>
             </svg>
 
@@ -72,14 +72,14 @@ const LogIn = () => {
               viewBox="0 0 24 24"
             >
               <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
                 stroke="currentColor"
+                strokeWidth="2"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                <circle cx="12" cy="8" r="4" />
+                <path d="M4 20c0-4 3.5-7 8-7s8 3 8 7" />
               </g>
             </svg>
 
